1b7ce08e5374bcd52d096d38bf1e1027af528a9c
kimnayoung <kny200208@naver.com>
2024-01-16 19:17:56 +0900

    Update AsapProjectBackkApplication.java

--- asap-project_backk/src/main/java/asap/asapproject_backk/AsapProjectBackkApplication.java
+++ asap-project_backk/src/main/java/asap/asapproject_backk/AsapProjectBackkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AsapProjectBackkApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AsapProjectBackkApplication.class, args);
 	}
 
