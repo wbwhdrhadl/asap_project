443fa7288b0700c6f92962b8d43c6587f0cfc645
kimnayoung <kny200208@naver.com>
2024-01-16 19:29:05 +0900

    new

---
+++ src/main/java/asap/asapproject_backk/AsapProjectBackkApplication.java
@@ -0,0 +1,13 @@
+package asap.asapproject_backk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsapProjectBackkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsapProjectBackkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/asap/asapproject_backk/AsapProjectBackkApplicationTests.java
@@ -0,0 +1,13 @@
+package asap.asapproject_backk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsapProjectBackkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
